e4ab1ac104474f1cf5d0fbf1c3a481d31c28ef7d
stana <stanislavalo@gmail.com>
2019-06-15 21:00:06 +0200

    glue code to make everything work

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
-    <v-app>
-    <v-container :fluid = "fluidContenair" >
-        <div class="row">
-            <div class="com-xs-12">
-                <app-header></app-header>
-                <router-view></router-view>
-            </div>
-        </div>
-    </v-container>
-    </v-app>
+  <v-container id="app" :fluid = "fluidContenair" >
+    <div class="row">      
+      <app-header></app-header>
+      <router-view></router-view>
+    </div>
+  </v-container> 
 </template>
 
 <script>
 import Header from './components/header/Header.vue';
 export default {
-    data() {
-        return{
-            fluidContenair:true
-        }
-    },
-    mounted () {
-        console.log(this.$vuetify.breakpoint);
-        this.fluidContenair = this.$vuetify.breakpoint.lgAndDown;
-        console.log("fluidContenair=" + this.fluidContenair);
-    },
-    components:{
-        appHeader:Header,
-    },
-    created(){
+  data() {
+    return{
+        fluidContenair:true
     }
+  },
+  mounted () {
+    this.fluidContenair = this.$vuetify.breakpoint.lgAndDown;
+  },
+  components:{
+    appHeader:Header,
+  },
+  created(){
+  }
 }
 </script>
+
